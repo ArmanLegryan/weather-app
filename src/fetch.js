ff83a605f3deb7ebcbe9ca60b265cf4b2e543130
ArmamLegryan <legryan@gmial.com>
2020-07-12 19:13:46 +0400

    commit

--- src/fetch.js
+++ src/fetch.js
@@ -2,6 +2,8 @@ export function fetchFunction() {
   let name = document.querySelector(".name");
   let desc = document.querySelector(".desc");
   let temp = document.querySelector(".temp");
+  let min_temp = document.querySelector(".min_temp");
+  let max_temp = document.querySelector(".max_temp");
 
   fetch(`https://api.openweathermap.org/data/2.5/weather?q=${select.value}&appid=365d86f409eb1f872c4e1683c3478afa
     `)
@@ -10,9 +12,14 @@ export function fetchFunction() {
       let nameVal = data["name"];
       let descVal = data["weather"][0]["description"];
       let tempVal = data["main"]["temp"];
+      let minTempVal = data["main"]["temp_min"];
+      let maxTempVal = data["main"]["temp_max"];
 
       name.innerHTML = nameVal;
       desc.innerHTML = descVal;
       temp.innerHTML = tempVal;
+
+      min_temp.innerHTML = `Min temp: ${minTempVal}`;
+      max_temp.innerHTML = `Max temp: ${maxTempVal}`;
     });
 }
